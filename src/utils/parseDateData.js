1bf943c717d71c3af76f7d5e180377a5099837e9
Chris Pond <chris@chrispond.com>
2020-11-30 17:13:45 -0700

    testing some date stuff

--- src/utils/parseDateData.js
+++ src/utils/parseDateData.js
@@ -1,9 +1,10 @@
 module.exports = (date = new Date()) => {
   const epoch = Math.floor(date.getTime() / 1000);
-  const UTCTime = new Date(date.toISOString());
-  const pretty = `${UTCTime.getFullYear()}/${
-    UTCTime.getMonth() + 1
-  }/${UTCTime.getDate()}`;
-
+  const timeZoneOffset = date.getTimezoneOffset();
+  const localTime = new Date(date.toISOString());
+  const pretty = `${localTime.getFullYear()}/${
+    localTime.getMonth() + 1
+  }/${localTime.getDate()}`;
+  console.log("--- Date:", date, timeZoneOffset, localTime);
   return { epoch, pretty, string: UTCTime };
 };
