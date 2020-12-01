24a11533c74e578781ebe05d916503bf8c7a14f9
Chris Pond <chris@chrispond.com>
2020-11-30 19:19:27 -0700

    Fixed timezoneoffset

--- src/utils/parseDateData.js
+++ src/utils/parseDateData.js
@@ -1,5 +1,5 @@
 module.exports = (date = new Date()) => {
-  const timeZoneOffset = -(420 * 60); // Mountain Standard Time offset from UTC
+  const timeZoneOffset = -(420 * 60000); // Mountain Standard Time offset from UTC
   const localTime = new Date(date.getTime() + timeZoneOffset);
   const epoch = Math.floor(localTime / 1000);
   const pretty = `${localTime.getFullYear()}/${
